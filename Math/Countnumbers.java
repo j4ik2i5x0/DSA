8e5768f8f853979476bca70bcddbb0063e41b66f
j4ik2i5x0 <jaikris24@gmail.com>
2025-06-10 20:36:33 +0530

    CountNos Added

---
+++ Math/Countnumbers.java
@@ -0,0 +1,17 @@
+package Math;
+
+import java.util.Scanner;
+
+public class Countnumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        int count = 0;
+        while(n > 0){
+            n = n / 10;  // Divide by 10 to remove last digit
+            count++;
+        }
+        System.out.println("Number of digits: " + count);
+    }
+}
