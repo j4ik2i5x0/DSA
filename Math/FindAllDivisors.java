342504268329d467387e674e94436b4f674a2dee
j4ik2i5x0 <jaikris24@gmail.com>
2025-06-12 12:08:01 +0530

    FindAllDivisors code is Added

--- Math/FindAllDivisors.java
+++ Math/FindAllDivisors.java
@@ -6,12 +6,15 @@ public class FindAllDivisors {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a no: ");
         int n = sc.nextInt();
+        int sum = 0;
 
         for(int i = 1; i<=n; i++){
             if(n%i==0){
                 System.out.println(i);
+                sum += i;
             }
         }
+        System.out.println("Sum: "+ sum);
         sc.close();
     }
 }
