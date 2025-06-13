52f26619cb614606dcc62a69d48aaacf7072b78c
itadmin <itadmin@sist.com>
2025-06-13 12:07:59 +0530

    Arrays Files Added

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {14, 72, 59, 80, 3, 44, 68, 25};
+        int searchNo = 3;
+        for(int i: arr){
+            if(i == searchNo){
+                System.out.println("Found the num: "+i);
+            }
+        }
+    }
+}
