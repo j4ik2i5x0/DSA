43f722d7d975b205f0dd169e7f2245ced9f69543
itadmin <itadmin@sist.com>
2025-06-13 12:47:14 +0530

    Arrays Files Added

---
+++ Arrays/pushZeroToEnd.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class pushZeroToEnd {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        pushZerosToEnd(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void pushZerosToEnd(int[] arr){
+        int nonZeroIndex = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[nonZeroIndex] = arr[i];
+                nonZeroIndex++;
+            }
+        }
+        
+        while (nonZeroIndex < arr.length) {
+            arr[nonZeroIndex] = 0;
+            nonZeroIndex++;
+        }
+    }
+}
