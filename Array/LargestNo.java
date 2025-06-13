bc6d4ccabec850a42ee5e63c9369d2d8894b48db
j4ik2i5x0 <jaikris24@gmail.com>
2025-06-13 22:34:01 +0530

    Array files added

--- Array/LargestNo.java
+++ Array/LargestNo.java
@@ -1,4 +1,4 @@
-package Arrays;
+package Array;
 
 public class LargestNo{
     public static void main(String[] args){
---
+++ Array/RotateArray.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        int d = 2;
+
+        for(int i = 0; i < d; i++){
+            int fst = arr[0];
+            for(int j = 1; j < arr.length-1; j++){
+                arr[j-1] = arr[j];
+            }
+            arr[arr.length-1] = fst;
+        }
+
+        for(int a: arr) System.out.print(a + ", ");
+    }
+}
