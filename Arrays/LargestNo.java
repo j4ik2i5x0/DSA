e0b5ccb89635b49b6557e286298c9ad8a6f4b5b4
itadmin <itadmin@sist.com>
2025-06-13 11:48:44 +0530

    Arrays Files Added

---
+++ Arrays/LargestNo.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class LargestNo{
+    public static void main(String[] args){
+        int[] arr = {2,53,12,8,34,71,6};
+        int largest = Integer.MIN_VALUE;
+        for(int i: arr){
+            if(i>largest) largest = i;
+        }
+        System.out.println("Largest number is: "+largest);
+    }
+}
---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class SecondLargest {
+    public static void main(String[] args){
+        int[] arr = {2,53,12,8,34,71,6};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for(int i: arr){
+            if(i>largest) {
+                secondLargest = largest;
+                largest = i;
+            }
+            else if(i>secondLargest && i!=largest){
+                secondLargest = i;
+            }
+        }
+        System.out.println("Largest number is: "+secondLargest);
+    }
+}
