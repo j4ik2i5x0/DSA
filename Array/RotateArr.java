4e63e23739a499404aceaaba73a1c4fae418f7ec
j4ik2i5x0 <jaikris24@gmail.com>
2025-06-13 22:42:16 +0530

    Array files added

---
+++ Array/RotateArr.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class RotateArr {
+
+        public static void main(String[] args) {
+            int arr[] = {1,2,3,4,5,6};
+            int d = 2;
+    
+            for(int i = 0; i < d; i++){
+                int fst = arr[0];
+                for(int j = 0; j < arr.length-1; j++){
+                    arr[j] = arr[j+1];
+                }
+                arr[arr.length-1] = fst;
+            }
+    
+            for(int a: arr) System.out.print(a + ", ");
+        }
+    }
+    
+
+
+
