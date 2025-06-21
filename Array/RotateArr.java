b8948ef5634bef06535e81b55d900dcbd940927f
j4ik2i5x0 <jaikris24@gmail.com>
2025-06-21 19:09:25 +0530

    files added

--- Array/RotateArr.java
+++ Array/RotateArr.java
@@ -2,12 +2,12 @@ package Array;
 
 public class RotateArr {
         public static void main(String[] args) {
-            int arr[] = {1,2,3,4,5};
+            int arr[] = {1,2,3,4,5};   
             int d = 2;
 
             reverseArr(arr, 0, arr.length-1);
-            reverseArr(arr, 0, d-1);
-            reverseArr(arr, d, arr.length-1);
+            reverseArr(arr, 0, d);
+            reverseArr(arr, d+1, arr.length-1);
 
             for(int a: arr) System.out.print(a + ", ");
         }
