8a17ac977467d2d7aa5db00b7ea070f21332cd5d
j4ik2i5x0 <jaikris24@gmail.com>
2025-10-04 19:03:57 +0530

    Selection Sort Code Update

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -12,25 +12,23 @@ public class SelectionSort {
         selection_sort(arr, n);
     }
 
-    public static void selection_sort(int[] arr, int n){
-        
-        for(int i = 0; i < n-1; i++){
-            int min = i;
-            for(int j = i+1; j < n; j++) {
-                if(arr[j] < arr[min]) {
-                    min = j;
+    public static void selection_sort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i; 
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {  
+                    minIndex = j;
                 }
             }
-
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
+            
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
+
         System.out.println("After selection sort:");
-        for (int i : arr) {
-            System.out.print(i + " ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
-
-
 }
